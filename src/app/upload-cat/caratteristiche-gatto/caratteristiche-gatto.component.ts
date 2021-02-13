a043ef0cb18d573b62f68e2a2fff98b391225bfc
jhon <lopiccoloottavio@gmail.com>
2021-02-13 12:58:50 +0100

    add

--- src/app/upload-cat/caratteristiche-gatto/caratteristiche-gatto.component.ts
+++ src/app/upload-cat/caratteristiche-gatto/caratteristiche-gatto.component.ts
@@ -21,14 +21,20 @@ export class CaratteristicheGattoComponent {
   removable = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   fruitCtrl = new FormControl();
-  filteredFruits: Observable<string[]>;
-  fruits: string[] = ['Lemon'];
-  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  filteredCaratteri: Observable<string[]>;
+  caratteristiche: string[] = [];
+  allFruits: string[] = [
+    'tenerone',
+    'miagoloso',
+    'indifferente',
+    'cattivo',
+    'giocoso',
+  ];
 
   @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
   constructor() {
-    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+    this.filteredCaratteri = this.fruitCtrl.valueChanges.pipe(
       startWith(null),
       map((fruit: string | null) =>
         fruit ? this._filter(fruit) : this.allFruits.slice()
@@ -42,7 +48,7 @@ export class CaratteristicheGattoComponent {
 
     // Add our fruit
     if ((value || '').trim()) {
-      this.fruits.push(value.trim());
+      this.caratteristiche.push(value.trim());
     }
 
     // Reset the input value
@@ -54,15 +60,15 @@ export class CaratteristicheGattoComponent {
   }
 
   remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
+    const index = this.caratteristiche.indexOf(fruit);
 
     if (index >= 0) {
-      this.fruits.splice(index, 1);
+      this.caratteristiche.splice(index, 1);
     }
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
+    this.caratteristiche.push(event.option.viewValue);
     this.fruitInput.nativeElement.value = '';
     this.fruitCtrl.setValue(null);
   }
